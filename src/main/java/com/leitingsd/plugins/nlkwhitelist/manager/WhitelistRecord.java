aa3bd792c18df8357bfe118473cb30b17f4bed44
Hoshino Satoru <47239222+leitingsd@users.noreply.github.com>
2024-07-22 01:30:35 +0800

    1.0-SNAPSHOT

--- src/main/java/com/leitingsd/plugins/nlkwhitelist/manager/WhitelistRecord.java
+++ src/main/java/com/leitingsd/plugins/nlkwhitelist/manager/WhitelistRecord.java
@@ -1,46 +1,20 @@
 package com.leitingsd.plugins.nlkwhitelist.manager;
 
-import java.time.Instant;
-
 public class WhitelistRecord {
-    private final long id;
-    private final Instant time;
-    private final String player;
-    private final String operator;
-    private final String guarantor;
-    private final String train;
-    private final String description;
-    private final String deleteAt;
-    private final String deleteReason;
-    private final String deleteOperator;
+    private String player;
+    private String guarantor;
+    private String train;
+    private String description;
 
-    public WhitelistRecord(long id, Instant time, String player, String operator, String guarantor, String train, String description, String deleteAt, String deleteReason, String deleteOperator) {
-        this.id = id;
-        this.time = time;
+    public WhitelistRecord(String player, String guarantor, String train, String description) {
         this.player = player;
-        this.operator = operator;
         this.guarantor = guarantor;
         this.train = train;
         this.description = description;
-        this.deleteAt = deleteAt;
-        this.deleteReason = deleteReason;
-        this.deleteOperator = deleteOperator;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public Instant getTime() {
-        return time;
     }
 
     public String getPlayer() {
         return player;
-    }
-
-    public String getOperator() {
-        return operator;
     }
 
     public String getGuarantor() {
@@ -53,17 +27,5 @@ public class WhitelistRecord {
 
     public String getDescription() {
         return description;
-    }
-
-    public String getDeleteAt() {
-        return deleteAt;
-    }
-
-    public String getDeleteReason() {
-        return deleteReason;
-    }
-
-    public String getDeleteOperator() {
-        return deleteOperator;
     }
 }
