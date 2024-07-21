1b11a5becb13ab9700336a34564835790ce47feb
Hoshino Satoru <47239222+leitingsd@users.noreply.github.com>
2024-07-22 01:33:03 +0800

    1.0-SNAPSHOT

--- src/main/java/com/leitingsd/plugins/nlkwhitelist/listener/WhitelistListener.java
+++ src/main/java/com/leitingsd/plugins/nlkwhitelist/listener/WhitelistListener.java
@@ -8,20 +8,18 @@ import com.velocitypowered.api.proxy.Player;
 import net.kyori.adventure.text.Component;
 
 public class WhitelistListener {
-
     private final NLKWhitelist plugin;
-    private final WhitelistManager whitelistManager;
 
-    public WhitelistListener(NLKWhitelist plugin, WhitelistManager whitelistManager) {
+    public WhitelistListener(NLKWhitelist plugin) {
         this.plugin = plugin;
-        this.whitelistManager = whitelistManager;
     }
 
     @Subscribe
     public void onPlayerLogin(LoginEvent event) {
         Player player = event.getPlayer();
-        if (!whitelistManager.isPlayerWhitelisted(player.getUsername())) {
-            player.disconnect(Component.text("你不在白名单中，无法加入服务器。"));
+        String playerId = player.getUsername();
+        if (!WhitelistManager.isPlayerWhitelisted(playerId)) {
+            event.setResult(LoginEvent.ComponentResult.denied(Component.text("You are not whitelisted on this server.")));
         }
     }
 }
